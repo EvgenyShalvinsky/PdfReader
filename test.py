2db26f0d0cfa470ddaa39d371e38e3c39ac9e129
EvgenyShalvinsky <e.a.shalvinsky@gmail.com>
2025-03-20 20:05:30 +0300

    Merge remote-tracking branch 'origin/main'

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-import pytest
-from main import scan_pdf, read_text, read_code, pdf_path, out_path
+from main import convert_to_images, scan_pdf, read_text, read_code, pdf_path, out_path
+import os
 
-def test_text():
-
+def test_img_null():
+    convert_to_images(pdf_path, out_path)
+    assert os.path.getsize(out_path) > 0
 
 def test_scan():
     test_dict = {'Barcode_1': ' 1',
@@ -29,4 +30,5 @@ def test_scan():
                  'NOTES': 'inspection notes'}
     real_dict = scan_pdf(pdf_path, out_path)
     assert real_dict == test_dict
+    assert os.path.exists(out_path) == False
 
