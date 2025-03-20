8e67fdac4b0d26a302dcd7a929a5595c8da322c8
EvgenyShalvinsky <e.a.shalvinsky@gmail.com>
2025-03-20 19:59:38 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ test.py
@@ -0,0 +1,32 @@
+import pytest
+from main import scan_pdf, read_text, read_code, pdf_path, out_path
+
+def test_text():
+
+
+def test_scan():
+    test_dict = {'Barcode_1': ' 1',
+                 'Barcode_2': ' tst',
+                 'COMPANY': 'GRIFFON AVIATION SERVICES LLC',
+                 'PN': ' tst',
+                 'DESCRIPTION': ' PART',
+                 'LOCATION': ' 111',
+                 'RECEIVER': ' 9',
+                 'EXP DATE': ' 13.04.2022',
+                 'CERT SOURCE': ' wef',
+                 'SN': ' 123123',
+                 'CONDITION': ' FN',
+                 'UOM': ' EA',
+                 'PO': ' P101',
+                 'REC.DATE': ' 18.04.2022',
+                 'MFG': ' efwfe',
+                 'BATCH': ' 1',
+                 'REMARK': '',
+                 'TAGGED BY': ' ',
+                 'Qty': ' 1',
+                 'DOM': ' 13.04.2022',
+                 'LOT': ' 1',
+                 'NOTES': 'inspection notes'}
+    real_dict = scan_pdf(pdf_path, out_path)
+    assert real_dict == test_dict
+
